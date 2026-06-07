e81f986c50ba4e314277aa40f12cae5aa9800d28
Guilherme Laurindo <guilhermelaurindode@gmail.com>
2026-06-07 21:41:43 +0000

    Implementação das rotas

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -6,7 +6,14 @@ import indexRouter from './routes/index.js'
 import usersRouter from './routes/users.js'
 import categoriasRouter from './routes/categorias.js'
 
+import categoriasRouter from './routes/categorias.js'
+import usuariosRouter from './routes/usuarios.js'
+import tarefasRouter from './routes/tarefas.js'
+import itensTarefaRouter from './routes/itensTarefa.js'
+import notificacoesRouter from './routes/notificacoes.js'
+
 const app = express()
+app.use(express.json())
 
 app.use(logger('dev'))
 app.use(json())
@@ -15,8 +22,15 @@ app.use(cookieParser())
 
 
 app.use('/categorias', categoriasRouter)
+app.use('/usuarios', usuariosRouter)
+app.use('/tarefas', tarefasRouter)
+app.use('/itensTarefa', itensTarefaRouter)
+app.use('/notificacoes', notificacoesRouter)
+
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
 export default app
+
+
